d28257a19c827095fc77015009cc78f2a6cf54a8
sallasal <47078225+sallasal@users.noreply.github.com>
2021-04-16 20:38:23 +0300

    Add weighted OSA and tests for it

--- SpellChecker/src/test/java/domain/OptimalStringAlignmentTest.java
+++ SpellChecker/src/test/java/domain/OptimalStringAlignmentTest.java
@@ -29,26 +29,62 @@ public class OptimalStringAlignmentTest {
     }
 
     @Test
+    public void weightedRecognizesTransposition() {
+        double distance = this.osa.weightedOSA("elephant", "eelphant");
+        assertEquals(0.825, distance, 0.001);
+    }
+
+    @Test
     public void osaRecognizesSmallDeletionDistance() {
         int distance = this.osa.optimalStringAlignment("moose", "mose");
         assertEquals(1, distance);
     }
-    
+
+    @Test
+    public void weightedRecognizesSmallDeletionDistance() {
+        double distance = this.osa.weightedOSA("moose", "mose");
+        assertEquals(0.537, distance, 0.001);
+    }
+
     @Test
     public void osaRecognizesSmallAdditionDistance() {
         int distance = this.osa.optimalStringAlignment("hose", "horse");
         assertEquals(1, distance);
     }
-    
+
+    @Test
+    public void weightedRecognizesSmallAdditionDistance() {
+        double distance = this.osa.weightedOSA("hose", "horse");
+        assertEquals(0.796, distance, 0.);
+    }
+
     @Test
     public void osaRecognizesSmallSubstitionDistance() {
         int distance = this.osa.optimalStringAlignment("moose", "goose");
         assertEquals(1, distance);
     }
-    
+
     @Test
-    public void osaRecognizesSubstitutionAndTranspositionTogether() {
+    public void weightedRecognizesSmallSubstitionDistance() {
+        double distance = this.osa.weightedOSA("moose", "goose");
+        assertEquals(0.841, distance, 0.001);
+    }
+
+    @Test
+    public void osaRecognizesDeletionAndTranspositionTogether() {
         int distance = this.osa.optimalStringAlignment("unicorn", "uincon");
         assertEquals(2, distance);
+    }
+
+    @Test
+    public void weightedRecognizesDeletionAndTranspositionTogether() {
+        double distance = this.osa.weightedOSA("unicorn", "uincon");
+        assertEquals(1.362, distance, 0.001);
+    }
+    
+    @Test
+    public void weightedRecognizesSubstitutionAndAdditionTogether() {
+        double distance = this.osa.weightedOSA("oliphant", "uliphhant");
+        assertEquals(1.637, distance, 0.001);
     }
 }
